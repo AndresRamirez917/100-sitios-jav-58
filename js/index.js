856b3c4d132a87854bf65b9d550ca46d577402c0
AndresRamirez917 <andresmintic2021@gmail.com>
2024-11-01 15:25:03 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -1,40 +1,29 @@
-const resul = document.getElementById('result');
+const result = document.getElementById('result');
 const listItems = []
 async function getData() {
-    const res = await fetch('https://rickandmortyapi.com/api/character')
-  
-    const { results } = await res.json()
-    console.log( { results })
-    console.log(typeof(results))
-    const restultMap = results.map(elemento => Object.entries(elemento))
-    const resultsSlice = restultMap.slice(0,2)
-    //console.log(restultMap)
-    //console.log(typeof(restultMap))
-    // Clear results
-    //result.innerHTML = ''
-const claves = Object.keys(results)
-for(i = 0; i < claves.length; i++){
-let clave = claves[i]
-console.log(results[clave])
+  const res = await fetch('https://rickandmortyapi.com/api/character')
+
+  const  { results }  = await res.json()
+
+results.forEach(user => {
+  for(i = 0; i < 5; i++){
+    if(user.id == i){
+  const li = document.createElement('li')
+  listItems.push(li)
+  console.log(listItems)
+
+  li.innerHTML = `
+      <img src="${user.image}" alt="${user.name}">
+      <div class="user-info">
+        <h4>${user.name} </h4>
+      </div>
+    `
+
+    result.appendChild(li)
 }
-  
-    results.forEach(user => {
-      const li = document.createElement('li')
-      for(i = 0; i <= resultsSlice.length; i++){
-    
-        listItems.push(li)
-    
-        li.innerHTML = `
-            <img src="${user.image}" alt="${user.name}">
-             <div class="user-info">
-               <h4>${user.name}</h4>
-            </div>
-          `
-    
-          result.appendChild(li)
-      }
-    })
-  }
+}
+  })
+}
 
   getData()
 
